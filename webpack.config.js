81b02f62f334ac4562cd5654d2f92f76d0c61590
SpiderProjectDeveloper <developer@spiderproject.com>
2021-03-14 00:45:10 +0300

    The 1st commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+// webpack.config.js
+// var BomPlugin = require('webpack-utf8-bom');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+  ],
+  optimization: {
+    minimize: true,
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+			{
+				loader: 'style-loader',
+			}, 
+			{
+				loader: 'css-loader',
+			}
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+		options: {
+			attributes: false,
+		},
+      }
+    ]
+  }
+};
